10ec19ada8e283959bc4e31ce8c0b3fecbea66dd
HannahSantos <hannah.santos.441@ufrn.edu.br>
2026-04-14 11:52:21 -0300

    refactor: Switched from double to float for performance

--- Library/Vec3.cpp
+++ Library/Vec3.cpp
@@ -28,37 +28,37 @@ Vec3& Vec3::operator/=(const Vec3 &v) {
     return *this;
 }
 
-Vec3& Vec3::operator+=(const double d) {
+Vec3& Vec3::operator+=(const float d) {
     vector[0] += d;
     vector[1] += d;
     vector[2] += d;
     return *this;
 }
 
-Vec3& Vec3::operator-=(const double d) {
+Vec3& Vec3::operator-=(const float d) {
     vector[0] -= d;
     vector[1] -= d;
     vector[2] -= d;
     return *this;
 }
 
-Vec3& Vec3::operator*=(const double d) {
+Vec3& Vec3::operator*=(const float d) {
     vector[0] *= d;
     vector[1] *= d;
     vector[2] *= d;
     return *this;
 }
 
-Vec3& Vec3::operator/=(const double d) {
+Vec3& Vec3::operator/=(const float d) {
     vector[0] /= d;
     vector[1] /= d;
     vector[2] /= d;
     return *this;
 }
 
-double Vec3::length() { return std::sqrt(lenght_sqrd()); }
+float Vec3::length() { return std::sqrt(lenght_sqrd()); }
 
-double Vec3::length_sqrd() { 
+float Vec3::length_sqrd() { 
     return std::pow(vector[0], 2) + 
 	   std::pow(vector[1], 2) + 
 	   std::pow(vector[2], 2);
@@ -92,45 +92,45 @@ Vec3 Vec3::operator/(const Vec3& u, const Vec3& v){
 		      u[2] / v[2]);
 }
 
-Vec3 Vec3::operator+(double d, const Vec3& v){
+Vec3 Vec3::operator+(float d, const Vec3& v){
     return Vec3::Vec3(d + v[0], 
 		      d + v[1], 
 		      d + v[2]);
 }
 
-Vec3 Vec3::operator+(const Vec3& v, double d){
+Vec3 Vec3::operator+(const Vec3& v, float d){
     return d + v;
 }
 
-Vec3 Vec3::operator-(double d, const Vec3& v){
+Vec3 Vec3::operator-(float d, const Vec3& v){
     return Vec3::Vec3(d - v[0], 
 		      d - v[1], 
 		      d - v[2]);
 }
 
-Vec3 Vec3::operator-(const Vec3& v, double d){
+Vec3 Vec3::operator-(const Vec3& v, float d){
     return Vec3::Vec3(v[0] - d, 
 		      v[1] - d, 
 		      v[2] - d);
 }
 
-Vec3 Vec3::operator*(double d, const Vec3& v){
+Vec3 Vec3::operator*(float d, const Vec3& v){
     return Vec3::Vec3(d * v[0], 
 		      d * v[1], 
 		      d * v[2]);
 }
 
-Vec3 Vec3::operator*(const Vec3& v, double d){
+Vec3 Vec3::operator*(const Vec3& v, float d){
     return d * v;
 }
 
-Vec3 Vec3::operator/(const Vec3& v, double d){
+Vec3 Vec3::operator/(const Vec3& v, float d){
     return Vec3::Vec3(v[0] / d, 
 		      v[1] / d, 
 		      v[2] / d);
 }
 
-double Vec3::prod_escalar(const Vec3& u, const Vec3& v){
+float Vec3::prod_escalar(const Vec3& u, const Vec3& v){
     return u[0] * v[0] + 
 	   u[1] * v[1] + 
 	   u[2] * v[2];
--- Library/Vec3.h
+++ Library/Vec3.h
@@ -6,28 +6,28 @@
 
 class Vec3 {
 public :
-    double vector[3];
+    float vector[3];
 
-    Vec3() : vector{0,0,0} {}
-    Vec3(double v0, double v1, double v2) : vector{v0, v1, v2} {}
+    Vec3() : vector{0.0,0.0,0.0} {}
+    Vec3(float v0, float v1, float v2) : vector{v0, v1, v2} {}
     ~Vec3() = default;
 
     const Vec3& operator+() const { return *this; }
     Vec3 operator-() const { return Vec3(-vector[0], -vector[1], -vector[2]); }
-    double operator[](int i) const { return vector[i]; }
-    double& operator[](int i) { return vector[i]; };
+    float operator[](int i) const { return vector[i]; }
+    float& operator[](int i) { return vector[i]; };
     
     Vec3& operator+=(const Vec3 &v);
     Vec3& operator-=(const Vec3 &v);
     Vec3& operator*=(const Vec3 &v);
     Vec3& operator/=(const Vec3 &v);
-    Vec3& operator+=(const double d);
-    Vec3& operator-=(const double d);
-    Vec3& operator*=(const double d);
-    Vec3& operator/=(const double d);
+    Vec3& operator+=(const float d);
+    Vec3& operator-=(const float d);
+    Vec3& operator*=(const float d);
+    Vec3& operator/=(const float d);
 
-    double length();
-    double length_sqrd();
+    float length();
+    float length_sqrd();
 
     std::ostream& operator<<(std::ostream& out, const Vec3& v);
 
@@ -36,15 +36,15 @@ public :
     Vec3 operator*(const Vec3& u, const Vec3& v);
     Vec3 operator/(const Vec3& u, const Vec3& v);
     
-    Vec3 operator+(double d, const Vec3& v);
-    Vec3 operator+(const Vec3& v, double d);
-    Vec3 operator-(double d, const Vec3& v);
-    Vec3 operator-(const Vec3& v, double d);
-    Vec3 operator*(double d, const Vec3& v);
-    Vec3 operator*(const Vec3& v, double d); 
-    Vec3 operator/(const Vec3& v, double d);
+    Vec3 operator+(float d, const Vec3& v);
+    Vec3 operator+(const Vec3& v, float d);
+    Vec3 operator-(float d, const Vec3& v);
+    Vec3 operator-(const Vec3& v, float d);
+    Vec3 operator*(float d, const Vec3& v);
+    Vec3 operator*(const Vec3& v, float d); 
+    Vec3 operator/(const Vec3& v, float d);
 
-    double prod_escalar(const Vec3& u, const Vec3& v);
+    float prod_escalar(const Vec3& u, const Vec3& v);
     Vec3& prod_vec(const Vec3& u, const Vec3& v);
     Vec3& unit_vec(const Vec3& v);
 
